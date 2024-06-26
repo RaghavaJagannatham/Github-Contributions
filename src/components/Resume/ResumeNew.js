006112e6748029ab67dc43c437618aadc5c50570
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2024-06-26 11:28:57 +0530

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Raghava_resume_updated06.pdf";
+import pdf from "../../Assets/../Assets/Raghava_resume_updated20241.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
